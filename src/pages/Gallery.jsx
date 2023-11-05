2d356ab5d08c4dea560dda85c9aa721cf1793175
foy5al <mh.foysal.h@gmail.com>
2023-11-05 18:38:26 +0600

    update drag and drop

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import img1 from "../assets/images/image-1.webp";
 import img2 from "../assets/images/image-2.webp";
 import img3 from "../assets/images/image-3.webp";
@@ -6,117 +6,95 @@ import img4 from "../assets/images/image-4.webp";
 import img5 from "../assets/images/image-5.webp";
 import img6 from "../assets/images/image-6.webp";
 import img7 from "../assets/images/image-7.webp";
-import img8 from "../assets/images/image-8.webp";
-import img9 from "../assets/images/image-9.webp";
-import img10 from "../assets/images/image-10.jpeg";
-import img11 from "../assets/images/image-11.jpeg";
 
 const Gallery = () => {
-  const imgLoop = [
-    img1,
-    img2,
-    img3,
-    img4,
-    img5,
-    img6,
-    img7,
-    img8,
-    img9,
-    img10,
-    img10,
-    img11,
-  ];
-  const [message, setMessage] = useState("");
+  const [show, setShow] = useState(true);
+  const [checkedInp, setCheckedInp] = useState(true);
+  const [imgDivs, setImgDivs] = useState([
+    { id: 1, content: <img src={img1} alt="Image 1" /> },
+    { id: 2, content: <img src={img2} alt="Image 2" /> },
+    { id: 3, content: <img src={img3} alt="Image 3" /> },
+    { id: 4, content: <img src={img4} alt="Image 3" /> },
+    { id: 5, content: <img src={img5} alt="Image 3" /> },
+    { id: 6, content: <img src={img6} alt="Image 3" /> },
+    { id: 7, content: <img src={img7} alt="Image 3" /> },
+  ]);
 
-  const dragStart = (event) => {
-    console.log(event.target.id);
-    event.dataTransfer.setData("text1", event.target.id);
+  const handleDragStart = (e, id) => {
+    e.dataTransfer.setData("text/plain", id);
   };
 
-  const dragEnter = (event) => {
-    if (event.target.className === "droptarget") {
-      setMessage("Entered the dropzone");
-      event.target.style.backgroundColor = "lightblue";
-    }
+  const handleDragEnd = (e) => {
+    e.preventDefault();
   };
 
-  const dragLeave = (event) => {
-    if (event.target.className === "droptarget") {
-      setMessage("Left the dropzone");
-      event.target.style.backgroundColor = "";
-    }
+  // drop functionality
+  const handleDrop = (e, id) => {
+    e.preventDefault();
+    //find drag item
+    const draggedItemId = e.dataTransfer.getData("text/plain");
+    const draggedItem = imgDivs.find(
+      (div) => div.id === parseInt(draggedItemId)
+    );
+
+    //find drop item
+    const dropItem = imgDivs.find((div) => div.id === id);
+
+    //update image div
+    const updatedDivs = imgDivs.map((div) => {
+      if (div.id === draggedItem.id) {
+        return { ...div, content: dropItem.content };
+      }
+      if (div.id === dropItem.id) {
+        return { ...div, content: draggedItem.content };
+      }
+      return div;
+    });
+
+    setImgDivs(updatedDivs);
   };
 
-  const allowDrop = (event) => {
-    event.preventDefault();
+  //handle Delete function
+  const handleDelete = (e, id) => {
+    console.log(e, id);
+  };
+  const handleSelected = (e, id) => {
+    e.preventDefault();
+    setCheckedInp(!checkedInp);
   };
 
-  const drop = (event) => {
-    console.log(event.target.id, "drop");
-    event.dataTransfer.setData("text2", event.target.id);
-    event.preventDefault();
-    const data1 = event.dataTransfer.getData("text1");
-    const data2 = event.dataTransfer.getData("text2");
-    console.log(data1, data2);
-    event.target.appendChild(document.getElementById(data1));
-    //event.target.appendChild(document.getElementById(data2));
-  };
   return (
-    <div className="text-black grid grid-cols-5 p-10 gap-2">
-      {imgLoop.map((item, index) => (
-        <div
-          key={index}
-          id={index}
-          onDragEnter={dragEnter}
-          onDragLeave={dragLeave}
-          onDrop={drop}
-          onDragOver={allowDrop}
-          className={`w-full border-gray-30 ${
-            index === 0 ? " row-span-2 col-span-2" : ""
-          }`}
-        >
-          <img
-            id={index}
-            onDragStart={dragStart}
-            draggable="true"
-            className={`border border-2 rounded-md`}
-            src={item}
-            alt=""
-          />
-        </div>
-      ))}
-      <div className="border border-2 rounded-md text-black my-auto">
-        <p>Add Image</p>
-      </div>
-      {/* {" "}
-      <style>{`
-        .droptarget {
-          width: 100px; 
-          height: 35px;
-          padding: 10px;
-          border: 1px solid black;
-        }
-      `}</style>
-      <div
-        className=""
-        onDragEnter={dragEnter}
-        onDragLeave={dragLeave}
-        onDrop={drop}
-        onDragOver={allowDrop}
-      >
-        <p onDragStart={dragStart} draggable="true" id="dragtarget">
-          Drag me!
-        </p>
-      </div>
-      <br />
-      <div
-        className="droptarget"
-        onDragEnter={dragEnter}
-        onDragLeave={dragLeave}
-        onDrop={drop}
-        onDragOver={allowDrop}
-      ></div>
-      <p id="demo">{message}</p> */}
+    <div className="p-2 grid grid-cols-5 gap-2 text-black">
+      {imgDivs.length &&
+        imgDivs.map((div, index) => (
+          <>
+            <div
+              key={div.id}
+              content={div.content}
+              draggable
+              className={`text-left w-full border-gray-30 border border-2 rounded-md hover:  ${
+                index === 0 ? " row-span-2 col-span-2" : ""
+              }`}
+              onDragStart={(e) => handleDragStart(e, div.id)}
+              onDragOver={(e) => handleDragEnd(e)}
+              onDrop={(e) => handleDrop(e, div.id)}
+            >
+              {show && (
+                <>
+                  <input
+                    className="text-left"
+                    type="checkbox"
+                    checked={checkedInp}
+                    onChange={(e) => handleSelected(e, div.id)}
+                    id={div.id}
+                  />
+                  <label htmlFor={div.id}></label>
+                </>
+              )}
+              {div.content}
+            </div>
+          </>
+        ))}
     </div>
   );
 };
